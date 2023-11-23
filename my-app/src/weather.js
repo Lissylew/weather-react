955fb8bc4c841ca25eb466a8c92c20d53f843560
Arlene Lewis <94923979+Lissylew@users.noreply.github.com>
2023-11-23 11:13:03 -0400

    added audio spinner

--- my-app/src/weather.js
+++ my-app/src/weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from 'react-loader-spinner';
 
 export default function Weather(props){
     function handleResponse (response) {
@@ -8,9 +9,15 @@ export default function Weather(props){
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=8c78e9e7e9928cd1a2a6f923072c3dec&units=metric`;
 axios.get(apiUrl).then(handleResponse);
        return (
-    <div>
-    <h2>Hello from Weather</h2>
-    
-    </div>
-    );
+						
+							<Audio
+								height="80"
+								width="80"
+								radius="9"
+								color="green"
+								ariaLabel="loading"
+								wrapperStyle
+								wrapperClass
+							/>
+				);
 }
